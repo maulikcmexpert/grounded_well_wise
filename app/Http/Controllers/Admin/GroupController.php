0226a333bfb5db7c80444ce0d625448f92b0db05
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-26 17:36:37 +0530

    Update GroupController.php

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -205,7 +205,7 @@ class GroupController extends CoreController
      */
     public function store(GroupPostRequest $request)
     {
-        dd($request);
+
         try {
             DB::beginTransaction();
             $total_session = $request->total_session;
